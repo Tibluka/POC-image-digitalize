cbf85111278c2e169964408ecc14528b5f76b89e
Lucas Gomes <cprpc@hotmail.com>
2023-08-02 12:53:51 -0300

    ajustes

--- App.tsx
+++ App.tsx
@@ -1,118 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState, useEffect} from 'react';
+import {Image} from 'react-native';
+import DocumentScanner from 'react-native-document-scanner-plugin';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App: React.FC = () => {
+  const [scannedImage, setScannedImage] = useState();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const scanDocument = async () => {
+    // start the document scanner
+    const {scannedImages} = await DocumentScanner.scanDocument();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    // get back an array with scanned image file paths
+    if (scannedImages.length > 0) {
+      // set the img src, so we can view the first scanned image
+      setScannedImage(scannedImages[0]);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  useEffect(() => {
+    // call scanDocument on load
+    scanDocument();
+  }, []);
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  return <Image resizeMode="contain" source={{uri: scannedImage}}/>;
+};
 
 export default App;
--- src/pages/camera.tsx
+++ src/pages/camera.tsx
@@ -1,13 +1,75 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import DocumentScanner from 'react-native-document-scanner-plugin';
 
 const Camera: React.FC = () => {
+  const [scannedImage, setScannedImage] = useState();
+
+  const scanDocument = async () => {
+    // start the document scanner
+    const { scannedImages } = await DocumentScanner.scanDocument() as any;
+
+    // get back an array with scanned image file paths
+    if (scannedImages!.length > 0) {
+      // set the img src, so we can view the first scanned image
+      setScannedImage(scannedImages[0])
+    }
+  }
 
   return (
-    <View>
-      <Text>Teste Camera</Text>
+    <View style={{ flex: 1 }}>
+      {scannedImage != null ?
+        (
+          <View>
+            <Image
+              source={{ uri: scannedImage }} // Substitua a URL pela URL da sua imagem
+              resizeMode="contain" // Defina o modo de redimensionamento desejado, por exemplo, "contain" ou "cover"
+              style={styles.image}
+            />
+            <TouchableOpacity style={styles.sendImage}>
+              <Text style={{color: 'white'}}>Enviar por e-mail</Text>
+            </TouchableOpacity>
+          </View>
+        ) :
+
+        <TouchableOpacity style={styles.captureButton} onPress={() => scanDocument()} ></TouchableOpacity>
+      }
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginTop: 20,
+    width: 200,
+    height: 200,
+    borderRadius: 4,
+    alignSelf: 'center'
+  },
+  captureButton: {
+    width: 70,
+    height: 70,
+    backgroundColor: 'gray',
+    borderWidth: 5,
+    borderColor: '#cdcdcd',
+    borderRadius: 35,
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 20
+  },
+  sendImage: {
+    padding: 12,
+    borderRadius: 8,
+    backgroundColor: '#436EAD',
+    borderWidth: 1,
+    borderColor: '#6F97CA',
+    alignSelf: 'center',
+    marginTop: 16
+  }
+});
 export default Camera;
